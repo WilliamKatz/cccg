59722e40ac088d23bc88507677e20aa18930dddb
William Katz <wfk213@nyu.edu>
2019-01-06 17:00:12 -0800

    deployed onto firebase

---
+++ build/precache-manifest.4406cdced232b868aa77dabfc8418520.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ecd0b130e7397e498521",
+    "url": "/static/js/main.ecd0b130.chunk.js"
+  },
+  {
+    "revision": "74bfbc4625c81ea7676a",
+    "url": "/static/js/1.74bfbc46.chunk.js"
+  },
+  {
+    "revision": "ecd0b130e7397e498521",
+    "url": "/static/css/main.37240690.chunk.css"
+  },
+  {
+    "revision": "15a75594dda1b67d0387e13dfb3ebd8a",
+    "url": "/index.html"
+  }
+];
